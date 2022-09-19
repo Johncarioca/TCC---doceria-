4714151266b6f0ff76b98df4797fa0d9bc0e9c56
Jonatas quintanilha balga <Jonatasquintanilhaximbiu116@gmail.com>
2022-09-18 23:20:17 -0300

    area adm 80% completa. precisa de leves ajustes

--- site/src/pages/adm/areaAdm/index.js
+++ site/src/pages/adm/areaAdm/index.js
@@ -8,22 +8,19 @@ export default function AreaAdm() {
 
             <header className='header'>
 
-
-
                 <div className='div-1'>
-                    <img className='seta' src="/assets/image/seta.png" alt="" />
 
+                    <img className='seta' src="/assets/image/seta.png" alt="" />
 
                     <img className='logo' src="/assets/image/logoAreaAdm.png" alt="" />
                 </div>
-
                 <p> home</p>
 
             </header>
-
             <section className='J'>
 
                 <div className='div-2'>
+                    
                     <h1>Area administrativa</h1>
 
                     <div className='b'>
