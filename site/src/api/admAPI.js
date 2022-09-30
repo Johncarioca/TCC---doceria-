f2a08ebff2dbae4bfe86412c2b98bb2ece4b2029
NicolasGoncalves <nicolasbabilas11@gmail.com>
2022-09-30 10:29:09 -0300

    alterações backend

---
+++ site/src/api/admAPI.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL:'http://localhost:5000'
+})
+
+export async function NovoProduto(nome,peso,preco,sinopse,ingredientes,estoque,destaque,categoria){
+    const resposta=await api.post('/adm/cadastro',{
+        nome: nome ,
+        peso: peso,
+        preco: preco,
+        sinopse: sinopse,
+        ingredientes: ingredientes,
+        estoque: estoque,
+        destaque: destaque,
+        categoria: categoria
+    })
+
+    return resposta.data;
+};
+
+export async function ImagemProduto(id,imagem){
+    const formData= new FormData();
+    formData.append('imagem',imagem);
+
+    const resposta = await api.put(`/adm/${id}/imagem`, formData, {
+        headers:{
+            "content-type":"multipart/form-data"
+        },
+    });
+
+    return resposta.status;
+};
