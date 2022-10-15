7a26984922bf18a45c428a3ec37d9e7d50ff5e3c
nissaura <cnissara.alemida@outlook.com>
2022-10-15 13:09:38 -0300

    csslanding

--- site/src/pages/home/index.js
+++ site/src/pages/home/index.js
@@ -57,7 +57,7 @@ export default function Home() {
             </section>
 
             <section className='section2-landing-page'>
-                <div className='duasdivs2'>
+                <div className='d1'>
                     <div> 
                         <img className='img-section2' src="/assets/image/oi.png" alt=""></img>
                     </div>
@@ -67,6 +67,10 @@ export default function Home() {
                     <div className='div-p2'>
                         <p>Nosso cardápio é recheado de bolos e salgadinhos pra festa, tortas doces e salgadas, lanches de metro, kits completos para comemorações coletivas ou caixinhas individuais, comidinhas congeladas e uma grande variedade de docinhos.</p>
                     </div>
+                </div>
+                <div className='d2'>
+                    <img=
+
                 </div>
             </section>
 
